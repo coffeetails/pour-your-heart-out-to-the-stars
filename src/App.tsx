f10a2c8a21b91d983cd3ed930ea7849b249ee9d7
Monica Björk <m.bjork95@gmail.com>
2023-11-19 10:15:03 +0100

    Updated so the words float up after every space instead of row

--- src/App.tsx
+++ src/App.tsx
@@ -8,40 +8,53 @@ function App() {
 
   let checkScrollWidth = (event) => {
     let textBox:any = event.target;
+    // console.log(event);
     
     if(textBox.value.length == 1) {
       setTextBoxScrollWidth(textBox.scrollWidth);
     }
 
-    if(textBoxScrollWidth() != textBox.scrollWidth) {
       // Wait untill the word is finished
+      const words = textBox.value.split(' ');
+      words.forEach(word => {
+        return word;
+      });
+      setFloatingTextContent(words); 
+      
       if(textBox.value[textBox.value.length-1] == " ") {
-        const words = textBox.value.split(' ');
-        setFloatingTextContent(words); 
-        // The animation will be for each row of words
+        const wordElems = document.getElementsByClassName("floatingTextWord");
+        const lastWordElem = wordElems[wordElems.length-1];
+        
         anime({
-          targets: ["#floatingTextWrapperElem", ".floatingTextWord"],
+          // targets: ["#floatingTextWrapperElem", ".floatingTextWord"],
+          targets: ".floatingTextWord",
+          // targets: wordElems[wordElems.length],
           duration: 5000,
           translateY: -270,
           opacity: 0,
-          rotate: anime.stagger([-100, 100]), 
+          rotate:  function() {
+            return anime.random(-100, 100);
+          },
           easing: 'easeInOutQuad',
           loop: false,
         });
         
-        textBox.value = "";
+        if(textBoxScrollWidth() != textBox.scrollWidth) {
+          textBox.value = "";
+        }
       }
-    }
   }
+
 
   return (
     <>
     <header>
       <label for="textbox">Pour your heart out to the stars.</label>
-      <button>Read more</button>
+      <button>What is this?</button>
     </header>
     <main>
-      <input type="text" id="textbox" name="textbox" oninput={checkScrollWidth} />
+      <input type="text" id="textbox" name="textbox" oninput={checkScrollWidth}></input>
+      {/* <input type="text" id="textbox" name="textbox" oninput={checkScrollWidth} /> */}
       
       <div id="floatingTextWrapper">
         <For each={floatingTextContent()}>
