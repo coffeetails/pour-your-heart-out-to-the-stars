f86400cb8886ff621e287823b7ff3d51a9baa5d1
Monica Björk <m.bjork95@gmail.com>
2023-11-22 19:28:10 +0100

    Almost solved multipe rows, proper rendering left

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,44 @@ import './App.css'
 import anime from 'animejs/lib/anime.es.js';
 
 function App() {
-  const [floatingTextContent, setFloatingTextContent] = createSignal<string[]>();
+  const [floatingTextContent, setFloatingTextContent] = createSignal([[""]]);
   const [textBoxScrollWidth, setTextBoxScrollWidth] = createSignal<number>(0);
   let rowsOfText = 0;
-
+  let wordBuffer = [[]];
+  
   let checkScrollWidth = (event) => {
+    let words = [[]];
     let textBox:any = event.target;
     // console.log(event);
 
     if(textBox.value.length == 1) {
       setTextBoxScrollWidth(textBox.scrollWidth);
     }
+    if(rowsOfText >= 1) {
+      words = wordBuffer;
+    }
     
-    let words:string[] = [];
+    // let newWords = [];
     textBox.value.split(' ').forEach((word, i) => {
-      words.push(word);
+      // newWords.push(word);
+      
+      // undefined when rowsOfText = 1;
+      words[rowsOfText].push(word);
     });
-    console.log("words",words);
+    
+    // words[rowsOfText] = newWords;
 
+    // setFloatingTextContent(newWords);
     setFloatingTextContent(words);
+    console.log("floatingTextContent()", floatingTextContent());
     
     if(textBox.value[textBox.value.length-1] == " ") {
       const wordElems = document.querySelectorAll(".floatingTextWord");
       const lastWordElem = wordElems[wordElems.length-1];
-
+      
       anime({
-        // targets: ".floatingTextWord",
-        targets: wordElems,
+        targets: ".floatingTextWord",
+        // targets: wordElems,
         // targets: lastWordElem,
         duration: 5000,
         opacity: 0,
@@ -51,9 +62,12 @@ function App() {
       });
       
       if(textBoxScrollWidth() != textBox.scrollWidth) {
-        textBox.value = "";
         ++rowsOfText;
-        console.log(rowsOfText);
+        // wordBuffer = newWords;
+        wordBuffer = words;
+        wordBuffer.push([]);
+        textBox.value = "";
+        // console.log(rowsOfText);
       }
     }
   }
@@ -67,18 +81,31 @@ function App() {
     </header>
     <main>
       <input type="text" id="textbox" name="textbox" oninput={checkScrollWidth}></input>
-      {/* <input type="text" id="textbox" name="textbox" oninput={checkScrollWidth} /> */}
       
       <div id="floatingTextWrapper">
-        <For each={floatingTextContent()}>
+        <For each={floatingTextContent()[floatingTextContent().length-1]}>
           {(word) => (
             <span class="floatingTextWord">{word}</span>
-            )}
+          )}
         </For>
+        {/* <For each={floatingTextContent()[1]}>
+          {(word) => (
+            <span class="floatingTextWord">{word}</span>
+          )}
+        </For> */}
+        {/* <For each={floatingTextContent()}>
+          {(row) => ( 
+            <For each={row}>
+              {(word) => (
+              <span class="floatingTextWord">{word}</span>
+              )}
+            </For>
+          )}
+        </For> */}
       </div>
     </main>
     </>
   )
 }
 
-export default App
+export default App;
